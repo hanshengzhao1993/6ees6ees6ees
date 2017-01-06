66d870085219a2042088eba54390325b5a21fa51
Han <hanshengzhao1993@gmail.com>
2017-01-06 10:53:59 -0800

    ES6 beebeebees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  // super.constructor () {
+  // 	this.age = 5;
+  // }
+  constructor () {
+  	super();
+  	// super.constructor(age)
+  	// this.age = 5;
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
+  
 };
